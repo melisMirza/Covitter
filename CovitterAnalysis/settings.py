f26898f958fcccac4ac87ccad797eb1a460528eb
melisMirza <39213053+melisMirza@users.noreply.github.com>
2020-12-25 16:15:06 +0300

    Update settings.py

--- CovitterAnalysis/settings.py
+++ CovitterAnalysis/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#&jd&)06_ohww4xf=)p=(tvv66(!#d!ke=cen_)i)@!klpnwb#'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
