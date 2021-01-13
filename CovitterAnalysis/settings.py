9b04e04e3e8c8bd65f7fe7917756d6d6833958b4
melismirza@gmail.com <melismirza@gmail.com>
2021-01-14 00:28:16 +0300

    heroku 3

--- CovitterAnalysis/settings.py
+++ CovitterAnalysis/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
-from boto.s3.connection import S3Connection
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
