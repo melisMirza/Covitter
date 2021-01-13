5badb2a4ac687f45d22fa279cc4066244fe5b5cd
melismirza@gmail.com <melismirza@gmail.com>
2021-01-13 23:51:10 +0300

    heroku setup

--- CovitterAnalysis/settings.py
+++ CovitterAnalysis/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CovitterAnalysis.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -96,7 +96,7 @@ DATABASES = {
         'PORT': 5432,
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
