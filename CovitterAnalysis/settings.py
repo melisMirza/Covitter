41be9e3cc6aa0e579ca4a52048e090e3dd907136
melismirza@gmail.com <melismirza@gmail.com>
2021-01-14 00:25:57 +0300

    heroku config 2

--- CovitterAnalysis/settings.py
+++ CovitterAnalysis/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env()
 % LOCAL TESTS%
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 '''
-SECRET_KEY = S3Connection(os.environ['DJANGO_SECRET_KEY'])
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
