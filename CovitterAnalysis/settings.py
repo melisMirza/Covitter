0c3c25796c63ab8fb9404307c9324a57db58bd30
melismirza@gmail.com <melismirza@gmail.com>
2021-01-14 16:30:25 +0300

    heroku db connection added

--- CovitterAnalysis/settings.py
+++ CovitterAnalysis/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -91,6 +92,9 @@ DATABASES = {
     }
 }
 
+# SWITCHED TO HEROKU POSTGRES DB
+heroku_db = dj_database_url.config()
+DATABASES['default'].update(heroku_db)
 '''
 DATABASES = {
     'default': {
